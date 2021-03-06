982513e110b82d5364fcf5cd9fc9667469b716c3
WeirderConcoctions <77578239+WeirderConcoctions@users.noreply.github.com>
2021-03-06 19:13:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 setTimeout(() => {
     const credit = prompt("Please enter the reason for your visiting this site")
-    alert(`Thank you! Credit Card Number : ${credit}`)
+    alert(`Thank you! Reason Given : ${credit}`)
+    alert(`Reason is invalid but what can we do, just go ahead and read`)
 }, 5000)
