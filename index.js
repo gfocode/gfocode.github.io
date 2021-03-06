669a3a959a05edd29c8e493a969fe22241cd4c94
WeirderConcoctions <77578239+WeirderConcoctions@users.noreply.github.com>
2021-03-06 19:12:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
-    const credit = prompt("Please enter your credit card number for being on this site for more than 5 seconds!")
+    const credit = prompt("Please enter the reason for your visiting this site")
     alert(`Thank you! Credit Card Number : ${credit}`)
 }, 5000)
